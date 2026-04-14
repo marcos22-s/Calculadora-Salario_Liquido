2821578550712a7b87c3c23869b0d19bbb2c1b3a
Marcos Firmino <marquinhosrodrig1022@gmail.com>
2026-04-14 00:06:50 -0300

    Add files via upload

---
+++ salario/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace salario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nome = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtsalario = new System.Windows.Forms.TextBox();
+            this.txtdescontos = new System.Windows.Forms.TextBox();
+            this.texadd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtliquido = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nome
+            // 
+            this.nome.AutoSize = true;
+            this.nome.Location = new System.Drawing.Point(135, 32);
+            this.nome.Name = "nome";
+            this.nome.Size = new System.Drawing.Size(33, 13);
+            this.nome.TabIndex = 0;
+            this.nome.Text = "nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "salario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(126, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "descontos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(128, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "adicionais";
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(188, 29);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(100, 20);
+            this.txtnome.TabIndex = 4;
+            this.txtnome.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtsalario
+            // 
+            this.txtsalario.Location = new System.Drawing.Point(188, 66);
+            this.txtsalario.Name = "txtsalario";
+            this.txtsalario.Size = new System.Drawing.Size(100, 20);
+            this.txtsalario.TabIndex = 5;
+            // 
+            // txtdescontos
+            // 
+            this.txtdescontos.Location = new System.Drawing.Point(188, 104);
+            this.txtdescontos.Name = "txtdescontos";
+            this.txtdescontos.Size = new System.Drawing.Size(100, 20);
+            this.txtdescontos.TabIndex = 6;
+            // 
+            // texadd
+            // 
+            this.texadd.Location = new System.Drawing.Point(188, 150);
+            this.texadd.Name = "texadd";
+            this.texadd.Size = new System.Drawing.Size(100, 20);
+            this.texadd.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 205);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "salario liquido";
+            // 
+            // txtliquido
+            // 
+            this.txtliquido.Location = new System.Drawing.Point(207, 205);
+            this.txtliquido.Name = "txtliquido";
+            this.txtliquido.Size = new System.Drawing.Size(100, 20);
+            this.txtliquido.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtliquido);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.texadd);
+            this.Controls.Add(this.txtdescontos);
+            this.Controls.Add(this.txtsalario);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nome);
+            this.Name = "Form1";
+            this.Text = "Salario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtsalario;
+        private System.Windows.Forms.TextBox txtdescontos;
+        private System.Windows.Forms.TextBox texadd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtliquido;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ salario/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace salario
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Funcionario func = new Funcionario(txtnome.Text, float.Parse(txtsalario.Text),float.Parse(txtdescontos.Text),float.Parse(texadd.Text));
+            txtliquido.Text = Convert.ToString(func.calcular(float.Parse(txtsalario.Text), float.Parse(txtdescontos.Text), float.Parse(texadd.Text)));
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
